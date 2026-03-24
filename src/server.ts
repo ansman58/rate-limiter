18936897bb9dfff393e43ed8b5dd2855b15b3e51
ansman58 <javanslem@gmail.com>
2026-03-24 17:21:16 +0100

    fix: use iife to start server

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { buildApp } from './app';
 const PORT = parseInt(process.env.PORT || '3000', 10);
 const HOST = process.env.HOST || '0.0.0.0';
 
-async function start(): Promise<void> {
+(async function start(): Promise<void> {
   const app = await buildApp();
 
   try {
@@ -14,6 +14,6 @@ async function start(): Promise<void> {
     app.log.error(err);
     process.exit(1);
   }
-}
+})();
 
-start();
+// start();
